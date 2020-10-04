f9daeaeb58db1ace1ac299ccfef2c9f3bbf7e41c
Rachel Bauer <rachel.bauer1@gmail.com>
2020-10-03 20:10:06 -0400

    first commit

---
+++ DataEntryPortal.py
@@ -0,0 +1,15 @@
+#Participant demographics
+print("What is your name?  ")
+name = input()
+
+print(f"Hello " + name + "how old are you?  ")
+age = input()
+
+
+#Participant attitudes towards 
+
+#Participant attitudes towards 
+
+#Participant attitudes towards 
+
+#Participant attitudes towards 
