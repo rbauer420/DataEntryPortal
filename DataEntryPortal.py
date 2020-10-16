76a841ab1f4252dc3f2e0136aff131c5f43b30b5
Rachel Bauer <rachel.bauer1@gmail.com>
2020-10-16 16:33:03 -0400

    Defined classes, tried fixing navigation

--- DataEntryPortal.py
+++ DataEntryPortal.py
@@ -19,12 +19,19 @@ def editForm():
     formSection = input('\nWhich section would you like to change?'
         '\n1. Demographics'
         '\n2. Workplan Steps'
-        '\n2. Populations served'
-        '\n2. Successes and Barriers'
-        '\n3. None\n\t')
-    #if:
-    #elif:
-    #else:
+        '\n3. Populations Served'
+        '\n4. Successes and Barriers'
+        '\n5. I do not wish to edit my data\n\t')
+    if message.lower() == '1' or message == 'demographics':
+        demographics(self) 
+    elif message.upper() == '2' or message == 'workplan steps':
+        workPlan()
+    elif message.upper() == '3' or message == 'populations served':
+        popServed()
+    elif message.upper() == '4' or message == 'successes and barriers':
+        successBarriers()
+    else:
+        pass
 
 #(Data Entry Portal Instructions and Table of Contents. Prompts for the user on how to navigate and exit out of the application.)
 print(f'\nWelcome to the Data Entry Portal, {userName}! '
@@ -43,13 +50,13 @@ while tableOfContents == True:
     message = input(prompt)
 
     if message.upper() == 'STEP 1' or message == '1':
-        editForm.demos()
+        editForm()
     elif message.upper() == 'STEP 2' or message == '2':
-        workPlan()
+        editForm()
     elif message.upper() == 'STEP 3' or message == '3':
-        popServed()
+        editForm()
     elif message.upper() == 'STEP 4' or message == '4':
-        successBarriers()
+        editForm()
     elif message.upper() == 'EXIT' or message == '5':
         print('Thank you for using the Data Entry Portal. Have a great day!\n')
         menu = False
@@ -71,6 +78,15 @@ while tableOfContents == True:
 
 #Python class demographics: date, agency, POC, county served, intervention name => Q1 - 5
 #class demographics:
+
+class demographics:
+    def __init__(self, dateEntered, agencyEntered, pointOfContactEntered, countyEntered, interventionEntered):
+        self.dateEntered = dateEntered
+        self.agencyEntered = agencyEntered
+        self.pointOfContactEntered = pointOfContactEntered
+        self.countyEntered = countyEntered
+        self.interventionEntered = interventionEntered
+
 dateSubmitted = input("1. Please enter today's date: " "  ")
 dateEntered = dateSubmitted
 
@@ -103,10 +119,19 @@ interventionEntered = interventionName
 
 #Python class workPlan, overall implementation progress
 
-implementationProgress = input(f'6. Please select which response option best describes your implementation progress for the intervention, {interventionEntered} '
+class workPlan:
+    def __init__(self, implementationProgressEntered, targetPopId, haveIntvMaterials, impFidelity, collectedPrePost,haveSharedData):
+        self.implementationProgressEntered = implementationProgressEntered 
+        self.targetPopId = targetPopId 
+        self.haveIntvMaterials = haveIntvMaterials 
+        self.impFidelity = impFidelity 
+        self.collectedPrePost = collectedPrePost 
+        self.haveSharedData = haveSharedData
+
+    implementationProgress = input(f'6. Please select which response option best describes your implementation progress for the intervention, {interventionEntered} '
                               '\nA. No activities B. Planning activities only. C. Implementation/maintenance activities:  ')
 
-implementationProgressEntered = implementationProgress
+    implementationProgressEntered = implementationProgress
     
     #if "no activities":
         #skip to class successes_barriers
@@ -138,7 +163,10 @@ haveSharedData = shareData
             #If steps == 5, or if steps < 5, skip to class successes_barriers 
 
 
-#Python class populations_served: "Please enter the total number for each population served by each demographic category:
+#Python class populationServed: "Please enter the total number for each population served by each demographics category:
+class popServed:
+    pass
+
     #12. Age:
         #A.	Under 18: ___
         #B.	18 – 29: ___
@@ -158,7 +186,12 @@ haveSharedData = shareData
             #(Once user enters data, the program will display the entered data as a bar graph)
 
 
-#Python class successes_barriers: 
+#Python class successBarriers: 
+class successBarriers:
+    def __init__(self, successesEntered, barriersEntered):
+        self.successesEntered = successesEntered
+        self.barriersEntered = barriersEntered
+
 successes = input(f"15. Please describe what successes you have experienced in {countyEntered} county in the past six months?: " " ")
 successesEntered = successes
 
@@ -169,7 +202,7 @@ barriersEntered = barriers
 
 #Python class report:Once user reaches this point, the application will display all of the data entered by the user in  a summary report, including graphs. 
         
-    # Then the message will display, 
+# Then the message will display, 
 print(f"Thank you {userName} for submitting your form!")   
 
 
