079265cf07dd928bd11f6339073eacda1aa46a0b
Rachel Bauer <rachel.bauer1@gmail.com>
2020-10-19 16:18:25 -0400

    Added global variables and fixed menu

--- DataEntryPortal.py
+++ DataEntryPortal.py
@@ -14,15 +14,30 @@ else:
 askName = input("Welcome to the Data Entry Portal! Can I have your " "name:  ")
 userName = askName
 
+#Global variables 
+def globalUserName():
+    global userName
+    userName = askName
+
+#countyEntered = countyEntered
+def globalCountyEntered():
+    global countyEntered
+    globalCountyEntered = globalCountyEntered
+
+#interventionEntered = interventionEntered
+def globalInterventionEntered():
+    global interventionEntered
+    globalInterventionEntered = globalInterventionEntered
+
 
 #Classes
 class demographics:
-    def __init__(self, dateEntered, agencyEntered, pointOfContactEntered, countyEntered, interventionEntered):
+    def __init__(self, dateEntered, agencyEntered, pointOfContactEntered, globalCountyEntered, globalInterventionEntered):
         self.dateEntered = dateEntered
         self.agencyEntered = agencyEntered
         self.pointOfContactEntered = pointOfContactEntered
-        self.countyEntered = countyEntered
-        self.interventionEntered = interventionEntered
+        self.globalCountyEntered = globalCountyEntered
+        self.globalInterventionEntered = globalInterventionEntered
     
     @classmethod
     def from_input(cls):
@@ -30,8 +45,8 @@ class demographics:
             dateEntered = input("1. Please enter today's date: " "  "),
             agencyEntered = input("2. Please enter the name of your agency: " "  "),
             pointOfContactEntered = input("3. Please enter your agencies point of contact: " "  "),
-            countyEntered = input("4. Please enter the county served: " "  "),
-            interventionEntered  = input("5. Please enter the name of the intervention implemented: " "  "),
+            globalCountyEntered = input("4. Please enter the county served: " "  "),
+            globalInterventionEntered  = input("5. Please enter the name of the intervention implemented: " "  "),
         )
 
 class workPlan:
@@ -46,7 +61,7 @@ class workPlan:
     @classmethod
     def from_input(cls):
         return cls(
-            implementationProgressEntered = input(f'6. Please select which response option best describes your implementation progress for the intervention, {interventionEntered} '
+            implementationProgressEntered = input(f'6. Please select which response option best describes your implementation progress for the intervention, {globalInterventionEntered} '
                                                   '\nA. No activities B. Planning activities only. C. Implementation/maintenance activities:  '),
             targetPopId = input("For the next five questions, please type “y” to report you have completed this step and “n” if you have not completed this workplan step."
                                 "\n7. Identified a target audience: " " "),
@@ -64,29 +79,9 @@ class successBarriers:
     @classmethod
     def from_input(cls):
         return cls(
-            successesEntered = input(f"15. Please describe what successes you have experienced in {countyEntered} county in the past six months?: " " "),
-            barriersEntered = input(f"16. Please describe what barriers you have experienced in {countyEntered} county in the past six months?: " " "),
+            successesEntered = input(f"15. Please describe what successes you have experienced in {globalInterventionEntered} county in the past six months?: " " "),
+            barriersEntered = input(f"16. Please describe what barriers you have experienced in {globalInterventionEntered} county in the past six months?: " " "),
         )
-
-
-#Allows user to edit form
-def editForm():
-    formSection = input('\nWhich section would you like to change?'
-        '\n1. Demographics'
-        '\n2. Workplan Steps'
-        '\n3. Populations Served'
-        '\n4. Successes and Barriers'
-        '\n5. I do not wish to edit my data\n\t')
-    if message.lower() == '1' or message == 'demographics':
-        demographics(demographics.from_input()) 
-    elif message.upper() == '2' or message == 'workplan steps':
-        workPlan(workPlan.from_input())
-    elif message.upper() == '3' or message == 'populations served':
-        popServed(self)
-    elif message.upper() == '4' or message == 'successes and barriers':
-        successBarriers(successBarriers.from_input())
-    else:
-        pass
 
 
 #Data Entry Portal Instructions and Table of Contents. Prompts for the user on how to navigate and exit out of the application.
@@ -106,13 +101,13 @@ while tableOfContents == True:
     message = input(prompt)
 
     if message.upper() == 'STEP 1' or message == '1':
-        editForm()
+        var = demographics.from_input(), workPlan.from_input(), successBarriers.from_input()
     elif message.upper() == 'STEP 2' or message == '2':
-        editForm()
+        var = workPlan.from_input()
     elif message.upper() == 'STEP 3' or message == '3':
-        editForm()
+        var = popServed(self)
     elif message.upper() == 'STEP 4' or message == '4':
-        editForm()
+        var = successBarriers.from_input()
     elif message.upper() == 'EXIT' or message == '5':
         print('Thank you for using the Data Entry Portal. Have a great day!\n')
         menu = False
